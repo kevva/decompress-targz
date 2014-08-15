a6f95c03607e7ec6402fd4cc79a5cb968b526ad5
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-15 21:20:52 +0200

    Ignore directories
    Fixes kevva/download#23.

--- index.js
+++ index.js
@@ -29,28 +29,30 @@ module.exports = function (opts) {
                 return cb(err);
             })
             .on('entry', function (file) {
-                var chunk = '';
+                if (file.type !== 'Directory') {
+                    var chunk = '';
 
-                file.on('data', function (data) {
-                    chunk += data.toString();
-                });
+                    file.on('data', function (data) {
+                        chunk += data.toString();
+                    });
 
-                file.on('end', function () {
-                    chunk = new Buffer(chunk, 'utf8');
+                    file.on('end', function () {
+                        chunk = new Buffer(chunk, 'utf8');
 
-                    if (opts.strip) {
-                        var f = path.basename(file.path);
-                        var p = path.dirname(file.path.split('/'));
+                        if (opts.strip) {
+                            var f = path.basename(file.path);
+                            var p = path.dirname(file.path.split('/'));
 
-                        if (Array.isArray(p)) {
-                            p = p.slice(opts.strip).join(path.sep);
+                            if (Array.isArray(p)) {
+                                p = p.slice(opts.strip).join(path.sep);
+                            }
+
+                            file.path = path.join(p, f);
                         }
 
-                        file.path = path.join(p, f);
-                    }
-
-                    files.push({ contents: chunk, path: file.path });
-                });
+                        files.push({ contents: chunk, path: file.path });
+                    });
+                }
             })
             .on('end', function () {
                 decompress.files = files;
