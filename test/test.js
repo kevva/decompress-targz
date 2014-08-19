ce6cd7f9a3819cf38cf182bc40ba1a77d9b9d3b8
shinnn <snnskwtnb@gmail.com>
2014-08-19 21:51:38 +0900

    Add test for `strip` option

--- test/test.js
+++ test/test.js
@@ -1,13 +1,18 @@
-/*global describe, it */
+/*global afterEach, describe, it */
 'use strict';
 
 var assert = require('assert');
 var Decompress = require('decompress');
 var fs = require('fs');
 var path = require('path');
+var rimraf = require('rimraf');
 var targz = require('../');
 
 describe('tar()', function () {
+    afterEach(function (cb) {
+        rimraf(path.join(__dirname, 'tmp'), cb);
+    });
+
     it('should decompress a TAR.GZ file', function (cb) {
         var decompress = new Decompress();
 
@@ -18,6 +23,20 @@ describe('tar()', function () {
             .decompress(function (err) {
                 assert(!err);
                 assert(fs.existsSync(path.join(__dirname, 'tmp/test.jpg')));
+                cb();
+            });
+    });
+    
+    it('should strip path level using the `strip` option', function (cb) {
+        var decompress = new Decompress();
+
+        decompress
+            .src(path.join(__dirname, 'fixtures/test-nested.tar.gz'))
+            .dest(path.join(__dirname, 'tmp'))
+            .use(targz({ strip: 1 }))
+            .decompress(function (err) {
+                assert(!err);
+                assert(fs.existsSync(path.join(__dirname, 'tmp/test/test.jpg')));
                 cb();
             });
     });
