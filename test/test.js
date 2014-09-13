1fa872e320b1e3113482fe6760c95f5741ac6b80
Kevin Martensson <kevinmartensson@gmail.com>
2014-09-13 22:35:10 +0200

    Use vinyl-file for testing

--- test/test.js
+++ test/test.js
@@ -1,22 +1,18 @@
 'use strict';
 
-var File = require('vinyl');
-var fs = require('fs');
 var isJpg = require('is-jpg');
 var path = require('path');
+var read = require('vinyl-file').read;
 var targz = require('../');
 var test = require('ava');
 
 test('decompress a TAR.GZ file', function (t) {
     t.plan(2);
 
-    fs.readFile(path.join(__dirname, 'fixtures/test.tar.gz'), function (err, buf) {
+    read(path.join(__dirname, 'fixtures/test.tar.gz'), function (err, file) {
         t.assert(!err);
 
         var stream = targz();
-        var file = new File({
-            contents: buf
-        });
 
         stream.on('data', function (file) {
             t.assert(isJpg(file.contents));
@@ -29,13 +25,10 @@ test('decompress a TAR.GZ file', function (t) {
 test('strip path level using the `strip` option', function (t) {
     t.plan(3);
 
-    fs.readFile(path.join(__dirname, 'fixtures/test-nested.tar.gz'), function (err, buf) {
+    read(path.join(__dirname, 'fixtures/test-nested.tar.gz'), function (err, file) {
         t.assert(!err);
 
         var stream = targz({ strip: 1 });
-        var file = new File({
-            contents: buf
-        });
 
         stream.on('data', function (file) {
             t.assert(file.path === 'test.jpg');
