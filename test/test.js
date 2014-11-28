fa354adedc189a0195cf632b5e13c9b6dab1b599
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-28 16:42:43 +0100

    Show errors in tests

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ test('decompress a TAR.GZ file', function (t) {
 	t.plan(2);
 
 	read(path.join(__dirname, 'fixtures/test.tar.gz'), function (err, file) {
-		t.assert(!err);
+		t.assert(!err, err);
 
 		var stream = targz();
 
@@ -26,7 +26,7 @@ test('strip path level using the `strip` option', function (t) {
 	t.plan(3);
 
 	read(path.join(__dirname, 'fixtures/test-nested.tar.gz'), function (err, file) {
-		t.assert(!err);
+		t.assert(!err, err);
 
 		var stream = targz({ strip: 1 });
 
