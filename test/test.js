a1b42406187f9de0cb7fbd66549238494163dca1
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-10 18:06:00 +0200

    Use ava in tests

--- test/test.js
+++ test/test.js
@@ -1,56 +1,45 @@
-'use strict';
-var path = require('path');
-var bufferEqual = require('buffer-equal');
-var isJpg = require('is-jpg');
-var test = require('ava');
-var vinylFile = require('vinyl-file');
-var decompressTargz = require('../');
+import path from 'path';
+import isJpg from 'is-jpg';
+import test from 'ava';
+import {read} from 'vinyl-file';
+import decompressTargz from '../';
 
-test('decompress a TAR.GZ file', function (t) {
-	t.plan(2);
-
-	var file = vinylFile.readSync(path.join(__dirname, 'fixtures/test.tar.gz'));
-	var stream = decompressTargz();
+test('decompress a TAR.GZ file', async t => {
+	const file = await read(path.join(__dirname, 'fixtures/test.tar.gz'));
+	const stream = decompressTargz();
 
 	file.extract = true;
 
-	stream.on('data', function (file) {
-		t.assert(!file.stat.isDirectory());
-		t.assert(isJpg(file.contents));
+	stream.on('data', file => {
+		t.false(file.stat.isDirectory());
+		t.true(isJpg(file.contents));
 	});
 
 	stream.end(file);
 });
 
-test('strip path level using the `strip` option', function (t) {
-	t.plan(3);
-
-	var file = vinylFile.readSync(path.join(__dirname, 'fixtures/test-nested.tar.gz'));
-	var stream = decompressTargz({strip: 1});
+test('strip path level using the `strip` option', async t => {
+	const file = await read(path.join(__dirname, 'fixtures/test-nested.tar.gz'));
+	const stream = decompressTargz({strip: 1});
 
 	file.extract = true;
 
-	stream.on('data', function (file) {
-		t.assert(!file.stat.isDirectory());
-		t.assert(file.path === 'test.jpg', file.path);
-		t.assert(isJpg(file.contents));
+	stream.on('data', file => {
+		t.false(file.stat.isDirectory());
+		t.is(file.path, 'test.jpg');
+		t.true(isJpg(file.contents));
 	});
 
 	stream.end(file);
 });
 
-test('skip decompressing a non-TAR.GZ file', function (t) {
-	t.plan(1);
-
-	var file = vinylFile.readSync(__filename);
-	var stream = decompressTargz();
-	var contents = file.contents;
+test('skip decompressing a non-TAR.GZ file', async t => {
+	const file = await read(__filename);
+	const stream = decompressTargz();
+	const contents = file.contents;
 
 	file.extract = true;
 
-	stream.on('data', function (data) {
-		t.assert(bufferEqual(data.contents, contents));
-	});
-
+	stream.on('data', data => t.deepEqual(data.contents, contents));
 	stream.end(file);
 });
